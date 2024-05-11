8280c69d2c7094282589ff335886671b638b939a
riccacio <riccardo.pacini@stud.unifi.it>
2024-05-11 17:07:22 +0200

    navbar and header done

--- js/current-date.js
+++ js/current-date.js
@@ -1,8 +1,24 @@
 window.onload = function() {
-    var currentDate = new Date();
 
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var formattedDate = currentDate.toLocaleDateString('it-IT', options);
+    function LargeFormat(){
+        var currentDate = new Date();
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-    document.getElementById('current-date').textContent = formattedDate;
+        return currentDate.toLocaleDateString('it-IT', options);;
+    }
+
+    function SmallFormat(){
+        var currentDate = new Date();
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth() + 1;
+        var year = currentDate.getFullYear();
+
+        if(day < 10) day = '0' + day;
+        if(month < 10) month = '0' + month;
+
+        return day + '/' + month + '/' + year;
+    }
+
+    document.getElementById('spanLarge').textContent = LargeFormat();
+    document.getElementById('spanSmall').textContent = SmallFormat();
 }
