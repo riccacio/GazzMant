cfb77ea9c543adaa1bf0b7365529271393ead9ed
riccacio <riccardo.pacini@stud.unifi.it>
2024-05-13 16:19:32 +0200

    icons and images added, body update

--- js/current-date.js
+++ js/current-date.js
@@ -1,17 +1,17 @@
 window.onload = function() {
 
     function LargeFormat(){
-        var currentDate = new Date();
-        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        let currentDate = new Date();
+        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-        return currentDate.toLocaleDateString('it-IT', options);;
+        return currentDate.toLocaleDateString('it-IT', options);
     }
 
     function SmallFormat(){
-        var currentDate = new Date();
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth() + 1;
-        var year = currentDate.getFullYear();
+        let currentDate = new Date();
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
 
         if(day < 10) day = '0' + day;
         if(month < 10) month = '0' + month;
